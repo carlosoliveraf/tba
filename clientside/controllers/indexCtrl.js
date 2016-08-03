f077b3e5956bba9f0de3bc3c6d983f80737241d7
carloseduardoolivera <carloseduardoolivera@gmail.com>
2016-08-03 00:50:42 -0300

    release app rodando

--- clientside/controllers/indexCtrl.js
+++ clientside/controllers/indexCtrl.js
@@ -1,5 +1,10 @@
 angular.module("main").controller("indexCtrl", function ($scope, $http) {
 	        $scope.helloWorld = "Bem vindo meu amigo ";
 
+	        $scope.click = function(){
+	        	console.log("click1");
+	        };
+
+
 		
 });
