59ac0f6fe5bb32b6c4e745a133b4d9ab79b629ca
carloseduardoolivera <carloseduardoolivera@gmail.com>
2016-08-04 03:08:03 -0300

    a

--- app.js
+++ app.js
@@ -184,7 +184,7 @@ app.post('/charac', function (req, res) {
 	var owner = validator.trim(validator.escape(req.param('owner')));
 
 	//teste
-	characController.save(name, level, vocation, stamina, equipments, owner function(resp){
+	characController.save(name, level, vocation, stamina, equipments, owner function(resp)){
 		res.json(resp);
 	});
 
